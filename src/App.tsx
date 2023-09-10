159d2f7af8bd279210743831387b3fccf9176409
Hangyul Kim <mackbex@bigpi.co>
2023-09-11 01:07:34 +0900

    feat: axios part2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import Nav from "./components/Nav";
 import React from "react";
 import Banner from "./components/Banner";
+import Row from "./components/Row";
+import requests from "./api/requests";
+import styled from "styled-components";
+import Footer from "./components/Footer";
 
 export default function App() {
     return (
-    <div>
+    <AppContainer>
         <Nav />
         <Banner />
-    </div>
+        <Row title={"Trending Now"} id="TN" fetchUrl={requests.fetchTrending} isLargeRow={true}/>
+        <Row title={"Top Rated"} id="TR" fetchUrl={requests.fetchTopRated}/>
+        <Row title={"Action Movies"} id="AM" fetchUrl={requests.fetchActionMovies}/>
+        <Row title={"Comedy Movies"} id="TN" fetchUrl={requests.fetchComedyMovies}/>
+        <Row title={"Horror Movies"} id="TN" fetchUrl={requests.fetchHorrorMovies}/>
+        <Row title={"Romance Movies"} id="TN" fetchUrl={requests.fetchRomanceMovies}/>
+        <Row title={"Documentaries"} id="TN" fetchUrl={requests.fetchDocumentaries}/>
+        <Footer/>
+    </AppContainer>
     )
 }
+
+const AppContainer = styled.div`
+  background: #111;
+`
--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,10 +1,71 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "../api/axios";
+import styled from "styled-components";
+import "./Row.css"
 
-export default function Row() {
+interface RowData {
+    title: string,
+    id: string,
+    fetchUrl: string,
+    isLargeRow?: boolean
+}
+
+interface Movie {
+    title: string,
+    poster_path: string,
+    backdrop_path: string,
+    id: number
+}
+
+export default function Row({title, id, fetchUrl, isLargeRow = false}: RowData) {
+
+    const [movies, setMovies] = useState<Movie[]>([])
+
+    useEffect(() => {
+        fetchMovieData()
+    }, []);
+
+    const fetchMovieData = async () => {
+        const request = await axios.get(fetchUrl, {
+            // headers: {Authorization: `Bearer ${axios.to}`}
+        })
+        setMovies(request.data.results)
+    }
+
     return (
-        <div>
-
-        </div>
+        <section className={"row"}>
+            <h2>{title}</h2>
+            <div className={"slider"}>
+                <div className={"slider__arrow-left"} onClick={() => {
+                    const el = window.document.getElementById(id)
+                    if (!el) return;
+                    el.scrollLeft -= window.innerWidth - 80;
+                }}>
+                    <span className={"arrow"}>{"<"}</span>
+                </div>
+                <div id={id} className={"row__posters"}>
+                    {movies.map((movie) => (
+                        <img
+                            key={movie.id}
+                            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                            src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                            loading={"lazy"}
+                            alt={movie.title}
+                        />
+                    ))}
+                </div>
+                <div className={"slider__arrow-right"} onClick={() => {
+                    const el = window.document.getElementById(id)
+                    if (!el) return;
+                    el.scrollLeft += window.innerWidth - 80;
+                }}>
+                <span
+                    className={"arrow"}
+                >
+                    {">"}
+                </span>
+                </div>
+            </div>
+        </section>
     );
 }
- 
