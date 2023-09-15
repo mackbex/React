40bce769442d752ebee3ec8ec42bd0301c7a6fd1
Hangyul Kim <mackbex@bigpi.co>
2023-09-16 03:46:50 +0900

    feat: redux

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+type Props = {
+  value: number;
+  onIncrement: () => void;
+  onDecrement: () => void;
+}
+
+function App({value, onIncrement, onDecrement}: Props) {
+  return (
+    <p>
+      Clicked: {value} times
+      {' '}
+      <button onClick={onIncrement}>+</button>
+      {' '}
+      <button onClick={onDecrement}>-</button>
+    </p>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import {createStore} from "redux";
+import counter from "./reducers";
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+const store = createStore(counter)
+
+const render = () => root.render(
+    <App
+    value={store.getState()}
+    onIncrement={() => store.dispatch({type: 'INCREMENT'})}
+    onDecrement={() => store.dispatch({type: 'DECREMENT'})}
+    />
+);
+render()
+store.subscribe(render)
+
